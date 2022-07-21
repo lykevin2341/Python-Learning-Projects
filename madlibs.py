d59ff706ef3f4286b2b50c09e77c085d32c89e8f
lykevin2341 <103277611+lykevin2341@users.noreply.github.com>
2022-07-20 19:25:56 -0700

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,20 @@
+# # The whole project is based on string concantenation
+
+# # Different ways to use string concantenation
+# word = "" (this is the variable that will be used to represent the word being concatenated)
+# print("text" + word)
+# print("text" .format{word})
+# print(f"text {word}")
+
+character_Name = input("Character name ")
+species_Name = input("Gives species ")
+adjective1 = input("Adjective ")
+adjective2 = input("Adjective ")
+character_adjective1 = input("Adjective of character ")
+character_adjective2 = input("Adjective of character ")
+verb1 = input("verb ")
+verb2 = input("verb ")
+
+print(f"In a world filled with {adjective1} and {adjective2}, only one {species_Name} is able to take a stand. \
+Being both {character_adjective1} and {character_adjective2} our hero {character_Name} will {verb1} and {verb2} \
+until the world is safe")
